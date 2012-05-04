9e320b3b50a175840dece46690bf7547e1a8eace
Jeff Lunt <jefflunt@gmail.com>
2012-05-04 14:58:45 -0500

    Bumped version number everywhere.

--- load_enotis_authority.rb
+++ load_enotis_authority.rb
@@ -1,9 +1,8 @@
 require 'rubygems'
 
-gemjar = File.join(File.dirname(__FILE__), 'enotis-pluggable-auth-0.0.2.DEV.jar')
+gemjar = File.join(File.dirname(__FILE__), 'enotis-pluggable-auth-0.0.3.DEV.jar')
 fail "gemjar not found" unless File.exist?(gemjar)
 require gemjar
 require 'enotis_pluggable_auth'
 
 $suite_authorization_source = EnotisPluggableAuth::EnotisAuthority.new
-
