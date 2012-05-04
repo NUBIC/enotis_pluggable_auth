812bce8e1f66d76ad007a375f76e9ba32b78c84f
Jeff Lunt <jefflunt@gmail.com>
2012-05-04 14:51:44 -0500

    Modified the name the jar to load, to match the actual JAR name - amateur mistake - yeesh!

--- load_enotis_authority.rb
+++ load_enotis_authority.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 
-gemjar = File.join(File.dirname(__FILE__), 'enotis-pluggable-auth-0.0.1.DEV.jar')
+gemjar = File.join(File.dirname(__FILE__), 'enotis-pluggable-auth-0.0.2.DEV.jar')
 fail "gemjar not found" unless File.exist?(gemjar)
 require gemjar
 require 'enotis_pluggable_auth'
